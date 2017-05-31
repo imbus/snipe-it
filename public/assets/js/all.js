de4492a81e5dfd7d565fd3ece10f192422344754
snipe <snipe@snipe.net>
2017-05-31 05:23:26 -0700

    Updated compiled assets

--- public/assets/js/all.js
+++ public/assets/js/all.js
@@ -34924,6 +34924,38 @@ var Lightbox = (function ($) {
  * @return {IIFE}          Immediately invoked. Returns self.
  */
 
+var lineOptions = {
+
+        legend: {
+            position: "bottom"
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold",
+                    beginAtZero: true,
+                    maxTicksLimit: 5,
+                    padding: 20
+                },
+                gridLines: {
+                    drawTicks: false,
+                    display: false
+                }
+            }],
+            xAxes: [{
+                gridLines: {
+                    zeroLineColor: "transparent"
+                },
+                ticks: {
+                    padding: 20,
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold"
+                }
+            }]
+        }
+
+};
 
 var pieOptions = {
     //Boolean - Whether we should show a stroke on each segment
