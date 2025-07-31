ea3ec546e947b9296b78a50e3aa559f7efe94d15
Lucas <46531925+cz-lucas@users.noreply.github.com>
2025-07-31 15:43:29 +0200

    Moved selects from partitials/forms/edit in own partials

---
+++ resources/views/partials/select/dropdowns/location-select.blade.php
@@ -0,0 +1,14 @@
+<select class="js-data-ajax" data-endpoint="locations" data-placeholder="{{ trans('general.select_location') }}" name="{{ $fieldname }}" style="width: 100%" id="{{ $fieldname }}_location_select" aria-label="{{ $fieldname }}" {{ (isset($multiple) && ($multiple=='true')) ? " multiple='multiple'" : '' }}{!! ((isset($item)) && (Helper::checkIfRequired($item, $fieldname))) ? ' required ' : '' !!}>
+    @isset($selected)
+    @foreach($selected as $location_id)
+    <option value="{{ $location_id }}" selected="selected" role="option" aria-selected="true" role="option">
+        {{ (\App\Models\Location::find($location_id))->name }}
+    </option>
+    @endforeach
+    @endisset
+    @if ($location_id = old($fieldname, (isset($item)) ? $item->{$fieldname} : ''))
+    <option value="{{ $location_id }}" selected="selected" role="option" aria-selected="true" role="option">
+        {{ (\App\Models\Location::find($location_id)) ? \App\Models\Location::find($location_id)->name : '' }}
+    </option>
+    @endif
+</select>
