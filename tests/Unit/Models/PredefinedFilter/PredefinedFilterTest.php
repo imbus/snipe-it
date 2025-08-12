54dd9040f52779ec8ce63294fb6f12f6497d975d
Leon Zebelein <leonz@imbus.de>
2025-08-12 09:20:43 +0200

    introduced unit test for predefinedFilter

---
+++ tests/Unit/Models/PredefinedFilter/PredefinedFilterTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Tests\Unit\Models\PredefinedFilter;
+
+use App\Models\PredefinedFilter;
+use Tests\TestCase;
+
+class PredefinedFilterTest extends TestCase {
+
+  public function testSimpleTest() {
+    $this->assertEquals(2, 1 + 1);
+  }
+
+}
