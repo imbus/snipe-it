ac2c740d7cd48415630284f1188b490d3bd70d66
Lucas <46531925+cz-lucas@users.noreply.github.com>
2025-09-11 11:28:51 +0200

    Added dropdown to select predefined filter

--- app/Http/Controllers/Api/PredefinedFilterController.php
+++ app/Http/Controllers/Api/PredefinedFilterController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\PredefinedFilter;
-
+use App\Http\Transformers\SelectlistTransformer;
 
 
 class PredefinedFilterController extends Controller
@@ -14,16 +14,16 @@ class PredefinedFilterController extends Controller
     public function index(Request $request)
     {
         //$this->authorize('view', PredefinedFilter::class);
-        
+
         $filters = PredefinedFilter::
             orderBy('name')
-            ->get(['id','name']);
+            ->get(['id', 'name']);
 
         $viewableFilters = [];
 
         foreach ($filters as $filter) {
             //if ($filter->userHasPermission(auth()->user(), 'view') || $filter->created_by === auth()->user()->id) {
-                $viewableFilters[] = $filter;
+            $viewableFilters[] = $filter;
             //}
         }
 
@@ -36,7 +36,7 @@ class PredefinedFilterController extends Controller
 
         $filter = PredefinedFilter::find($id)
             //->where('created_by', $request->user()->id)
-            ;
+        ;
 
         if (!$filter) {
             return response()->json(['error' => 'Filter not found'], 404);
@@ -53,9 +53,9 @@ class PredefinedFilterController extends Controller
         $validated = $request->validate($rules);
 
         $predefined_filter = PredefinedFilter::create([
-            'name'=> $validated['name'],
-            'filter_data'=> $validated['filter_data'],
-            'created_by'=> $request->user()->id
+            'name' => $validated['name'],
+            'filter_data' => $validated['filter_data'],
+            'created_by' => $request->user()->id
         ]);
 
         // Weiterleitung zur Detailseite (oder Übersicht)?
@@ -68,19 +68,19 @@ class PredefinedFilterController extends Controller
     public function update(Request $request, int $id): JsonResponse
     {
         $filter = PredefinedFilter::find($id);
-        
+
         if (!$filter) {
             return response()->json(['error' => 'Filter not found'], 404);
         }
-        
+
         // $this->authorize('edit', PredefinedFilter::class);
 
         $rules = (new PredefinedFilter)->getRules();
         $validated = $request->validate($rules);
 
         $filter->update([
-            'name'=> $validated['name'],
-            'filter_data'=> $validated['filter_data'],
+            'name' => $validated['name'],
+            'filter_data' => $validated['filter_data'],
         ]);
 
         return response()->json([
@@ -88,8 +88,9 @@ class PredefinedFilterController extends Controller
             'filter_data' => $filter
         ], 200); // check for status code
     }
-    
-    public function destroy(Request $request, int $id){
+
+    public function destroy(Request $request, int $id)
+    {
         $filter = PredefinedFilter::find($id);
 
         // $this->authorize('delete', PredefinedFilter::class);
@@ -101,8 +102,34 @@ class PredefinedFilterController extends Controller
         $filter->delete();
 
         return response()->json([
-            'message'=> __('admin/reports/message.delete.success'),
-        ],200); //check for status code
+            'message' => __('admin/reports/message.delete.success'),
+        ], 200); //check for status code
     }
-    
+
+    public function selectlist(Request $request)
+    {
+        $this->authorize('view.selectlists');
+        $predefinedFilters = PredefinedFilter::select([
+            'id',
+            'name',
+        ]);
+
+        if ($request->filled('search')) {
+            $predefinedFilters = $predefinedFilters->where('name', 'LIKE', '%'.$request->get('search').'%');
+        }
+
+        $predefinedFilters = $predefinedFilters->orderBy('name', 'ASC')->paginate(50);
+
+        // Loop through and set some custom properties for the transformer to use.
+        // This lets us have more flexibility in special cases like assets, where
+        // they may not have a ->name value but we want to display something anyway
+        foreach ($predefinedFilters as $predefinedFiler) {
+            $predefinedFiler->use_text = $predefinedFiler->name;
+            //$manufacturer->use_image = ($manufacturer->image) ? Storage::disk('public')->url('manufacturers/'.$manufacturer->image, $manufacturer->image) : null;
+        }
+
+        return (new SelectlistTransformer)->transformSelectlist($predefinedFilters);
+    }
+
+
 }
