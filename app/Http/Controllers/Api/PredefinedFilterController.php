1fa4fe2892600f9fb58d8ae055d11f5cbd3cf987
JonasGoseberg <jonas.goseberg@imbus.de>
2025-09-16 08:21:42 +0200

    changed fixed l10n

--- app/Http/Controllers/Api/PredefinedFilterController.php
+++ app/Http/Controllers/Api/PredefinedFilterController.php
@@ -48,7 +48,7 @@ class PredefinedFilterController extends Controller
 
         if (!$filter) {
             return response()->json([
-            'message' => __('admin/reports/message.NotFound'), // TODO check for lang
+            'message' => ('admin/predefinedFilters/message.does_not_exist'),
         ], 404);
         }
 
@@ -61,7 +61,7 @@ class PredefinedFilterController extends Controller
         }
 
         return response()->json([
-            'message' => __('admin/reports/message.NotAllowed'), // TODO check for lang
+            'message' => trans('admin/predefinedFilters/message.show.not_allowed'), 
         ], 403);
     }
 
@@ -75,7 +75,7 @@ class PredefinedFilterController extends Controller
         if (!empty($validated['is_public'] ?? false)) {  
             if (!$user->hasAccess('predefinedFilter.create')) {
                 return response()->json([
-                    'message' => __('admin/reports/message.NotAllowed'), // TODO check for lang
+            'message' => trans('admin/predefinedFilters/message.create.not_allowed'), 
             ], 403);}
         }
 
@@ -115,19 +115,19 @@ class PredefinedFilterController extends Controller
             // private -> public requires create permission
             if (!$user->hasAccess('predefinedFilter.create')) {
                 return response()->json([
-                    'message' => __('admin/reports/message.NotAllowedToChangePublicStatus'),
+                    'message' => trans('admin/predefinedFilters/message.update.not_allowed_to_change_isPublic'),
                 ], 403);
             }
         }
         } elseif ($currentIsPublic) {
             if (!$filter->userHasPermission($user, 'update')) {
                 return response()->json([
-                    'message' => __('admin/reports/message.NotAllowed'),
+                    'message' => trans('admin/predefinedFilters/message.not_allowed_to_edit'),
                 ], 403);
             }
         } else {
             return response()->json([
-                'message' => __('admin/reports/message.NotAllowed'),
+                    'message' => trans('admin/predefinedFilters/message.not_allowed_to_edit'),
             ], 403);
         }
 
@@ -137,7 +137,7 @@ class PredefinedFilterController extends Controller
         $filter->save();
 
         return response()->json([
-            'message' => __('admin/reports/message.update.success'),
+            'message' => trans('admin/predefinedFilters/message.update.success'),
             'filter_data' => $filter,
         ], 200);
     }
@@ -149,7 +149,9 @@ class PredefinedFilterController extends Controller
         $filter = PredefinedFilter::find($id);
 
         if (!$filter) {
-            return response()->json(['error' => 'Filter not found'], 404);
+            return response()->json([
+            'message' => ('admin/predefinedFilters/message.does_not_exist'),
+        ], 404);
         }
 
         if ($filter->created_by === $user->id) {
@@ -157,19 +159,19 @@ class PredefinedFilterController extends Controller
         } elseif ($filter->is_public) {
             if (!$filter->userHasPermission($user, 'destroy')) {
                 return response()->json([
-                    'message' => __('admin/reports/message.NotAllowed'),
+                    'message' => __('admin/predefinedFilters/message.not_allowed_to_delete'),
                 ], 403);
             }
             $filter->delete();
         } else {
         
             return response()->json([
-                'message' => __('admin/reports/message.NotAllowed'),
+                'message' => __('admin/predefinedFilters/message.delete.not_allowed_to_delete'),
             ], 403);
         }
 
         return response()->json([
-            'message'=> __('admin/reports/message.delete.success'),
+            'message'=> __('admin/predefinedFilters/message.delete.success'),
         ],200);
     }
 }
