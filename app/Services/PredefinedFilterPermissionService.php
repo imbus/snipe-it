19a5e5844f002c124802dca5639489a7a704e3f8
Lucas <46531925+cz-lucas@users.noreply.github.com>
2025-09-22 12:01:33 +0200

    Added option to set/edit/delete predefined filter permssion groups over the api

---
+++ app/Services/PredefinedFilterPermissionService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\PredefinedFilterPermission;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+
+class PredefinedFilterPermissionService
+{
+    public function store(array $validated): PredefinedFilterPermission
+    {
+        $userId = Auth::id();
+
+        $permission = new PredefinedFilterPermission();
+        $permission->predefined_filter_id = $validated['predefined_filter_id'];
+        $permission->permission_group_id = $validated['permission_group_id'];
+        $permission->created_by = $userId;
+
+        if (! $permission->save()) {
+            throw new Exception($permission->getErrors());
+        }
+
+        return $permission;
+    }
+
+    public function show(int $id): PredefinedFilterPermission
+    {
+        return PredefinedFilterPermission::with('filter')->findOrFail($id);
+    }
+
+    public function delete(int $id): void
+    {
+        $permission = PredefinedFilterPermission::findOrFail($id);
+        $permission->delete();
+    }
+
+    public function deletePermissionByFilterId($filterId): void
+    {
+        $permissions = PredefinedFilterPermission::where('predefined_filter_id', '=', $filterId)->get();
+        foreach ($permissions as $permission) {
+            $permission->delete();
+        }
+    }
+
+    public function getPermissionsById(int $filterId)
+    {
+        return PredefinedFilterPermission::where('predefined_filter_id', '=', $filterId)->get();
+    }
+}
--- app/Services/PredefinedFilterService.php
+++ app/Services/PredefinedFilterService.php
@@ -3,13 +3,24 @@
 namespace App\Services;
 
 use App\Models\PredefinedFilter;
+use App\Services\PredefinedFilterPermissionService;
+use DB;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
+use Throwable;
 
 class PredefinedFilterService
 {
+    private PredefinedFilterPermissionService $predefinedFilterPermissionService;
+
+    public function __construct(PredefinedFilterPermissionService $predefinedFilterPermissionService)
+    {
+        $this->predefinedFilterPermissionService = $predefinedFilterPermissionService;
+    }
+
     public function getAllViewableFilters(): Collection
     {
         $user = Auth::user();
@@ -38,18 +49,28 @@ class PredefinedFilterService
     public function canUserViewFilter($filter): bool
     {
         $user = Auth::user();
+
         return $filter->created_by == $user->id ||
                ($filter->is_public && $filter->userHasPermission($user, 'view'));
     }
 
     public function createFilter(array $validated): PredefinedFilter
     {
-        return PredefinedFilter::create([
+        $filterCreateResponse = PredefinedFilter::create([
             'name' => $validated['name'],
             'filter_data' => $validated['filter_data'],
             'created_by' => Auth::id(),
             'is_public' => $validated['is_public'] ?? 0,
         ]);
+
+        if (array_key_exists('permissions', $validated)) {
+            foreach ($validated['permissions'] as $permission) {
+                $permission['predefined_filter_id'] = $filterCreateResponse->id;
+                $this->predefinedFilterPermissionService->store($permission);
+            }
+        }
+
+        return $filterCreateResponse;
     }
 
     public function updateFilter(PredefinedFilter $filter, array $validated): PredefinedFilter
@@ -57,9 +78,34 @@ class PredefinedFilterService
         $filter->fill([
             'name' => $validated['name'],
             'filter_data' => $validated['filter_data'],
-            'is_public' => $validated['is_public'],
+            'is_public' => $validated['is_public'] ?? $filter->is_public,
         ]);
         $filter->save();
+
+        if (array_key_exists('permissions', $validated)) {
+            $currentlySetPermissions = $this->predefinedFilterPermissionService->getPermissionsById($filter->id);
+            $newPermissions = $validated['permissions'];
+            $permissionDiff = $this->syncPermissions($currentlySetPermissions->toArray(), $newPermissions);
+
+            try {
+                DB::transaction(function () use ($permissionDiff, $filter) {
+                    if (!empty($permissionDiff['to_delete'])) {
+                        foreach ($permissionDiff['to_delete'] as $permission) {
+                            $this->predefinedFilterPermissionService->deletePermissionByFilterId($permission['predefined_filter_id']);
+                        }
+                    }
+
+                    if (!empty($permissionDiff['to_add'])) {
+                        foreach ($permissionDiff['to_add'] as $permission) {
+                            $permission['predefined_filter_id'] = $filter->id;
+                            $this->predefinedFilterPermissionService->store($permission);
+                        }
+                    }
+                });
+            } catch (Throwable $e) {
+                throw new Exception($e->getMessage());
+            }
+        }
 
         return $filter;
     }
@@ -73,7 +119,7 @@ class PredefinedFilterService
     {
         $user = Auth::user();
 
-        $filters = PredefinedFilter::with("permissionGroups")
+        $filters = PredefinedFilter::with('permissionGroups')
             ->orderBy('name')
             ->get(['id', 'name', 'created_by', 'is_public']);
 
@@ -103,5 +149,29 @@ class PredefinedFilterService
         }
 
         return $paginated;
+    }
+
+    private function syncPermissions($currentPermissions, $newPermissions): array
+    {
+        $toAdd = array_udiff(
+            $newPermissions,
+            $currentPermissions,
+            function ($obj_a, $obj_b) {
+                return $obj_a['permission_group_id'] !== $obj_b['permission_group_id'];
+            }
+        );
+
+        $toDelete = array_udiff(
+            $currentPermissions,
+            $newPermissions,
+            function ($obj_a, $obj_b) {
+                return $obj_a['permission_group_id'] !== $obj_b['permission_group_id'];
+            }
+        );
+
+        return [
+            'to_add' => $toAdd,
+            'to_delete' => $toDelete,
+        ];
     }
 }
