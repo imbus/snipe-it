7e44d38acff0d4c77277544e9e75e9ccc33136e7
JonasGoseberg <jonas.goseberg@imbus.de>
2025-09-26 13:25:28 +0200

    hotfix Controller Method Update

--- app/Http/Controllers/Api/PredefinedFilterController.php
+++ app/Http/Controllers/Api/PredefinedFilterController.php
@@ -98,11 +98,9 @@ class PredefinedFilterController extends Controller
             if (! $currentIsPublic && $newIsPublic && ! $user->hasAccess('predefinedFilter.create')) {
                 return response()->json(['message' => trans('admin/predefinedFilters/message.update.not_allowed_to_change_isPublic')], 403);
             }
-        } elseif ($currentIsPublic) {
-            if (! $filter->userHasPermission($user, 'update')) {
-                return response()->json(['message' => trans('admin/predefinedFilters/message.not_allowed_to_edit')], 403);
-            }
-        } else {
+        } elseif (! \->userHasPermission(\, 'update')) {
+            return response()->json(['message' => trans('admin/predefinedFilters/message.not_allowed_to_edit')], 403);
+        }
             return response()->json(['message' => trans('admin/predefinedFilters/message.not_allowed_to_edit')], 403);
         }
 
