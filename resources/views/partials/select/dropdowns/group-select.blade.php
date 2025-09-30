46a01e6d43af819e3ead5039dfe42fc9c2062b09
Lucas <46531925+cz-lucas@users.noreply.github.com>
2025-09-30 12:57:28 +0200

    Fixed error in dropdown

--- resources/views/partials/select/dropdowns/group-select.blade.php
+++ resources/views/partials/select/dropdowns/group-select.blade.php
@@ -1,42 +1,29 @@
-<select wire:ignore class="js-data-ajax livewire-select2 group-select" data-endpoint="groups"
-    data-placeholder="{{ trans('general.select_group') }}" data-livewire-target="groups" {{-- adjust to your Livewire
-    property name --}} name="{{ $fieldname }}" style="width:100%"
-    data-livewire-component="{{ $this->getId() }}""
-    id="{{ isset($select_id) ? $select_id : $fieldname . '_group_select' }}" {!! isset($item) && Helper::checkIfRequired($item, $fieldname) ? ' required' : '' !!} {{ isset($multiple) && $multiple == 'true' ? " multiple='multiple'" : '' }}
-    >
-
+<select class="js-data-ajax livewire-select2" data-endpoint="groups"
+    data-placeholder="{{ trans('general.select_group') }}" name="{{ $fieldname }}" style="width: 100%"
+    data-livewire-component=" {{ $this->getId() }}}}"
+    id="{{ isset($select_id) ? $select_id : $fieldname . '_group_select' }}" {!! isset($item) && Helper::checkIfRequired($item, $fieldname) ? ' required ' : '' !!}{{ isset($multiple) && $multiple == 'true' ? " multiple='multiple'" : '' }}>
     @isset($selected)
-        @php
-            if (!is_iterable($selected)) {
+        @if (!is_iterable($selected))
+            @php
                 $selected = [$selected];
-            }
-        @endphp
-        @foreach ($selected as $group_id)
-            @php $g = \App\Models\Group::find($group_id); @endphp
-            @if ($g)
-                <option value="{{ $group_id }}" selected="selected">{{ $g->name }}</option>
-            @endif
-        @endforeach
-    @endisset
-
-    @if ($group_id = old($fieldname, isset($item) ? $item->{$fieldname} : ''))
-        @php $g2 = \App\Models\Group::find($group_id); @endphp
-        @if ($g2)
-            <option value="{{ $group_id }}" selected="selected">{{ $g2->name }}</option>
+            @endphp
         @endif
-    @endif
-
-    @isset($otherOptions)
-        @php
-            if (!is_iterable($otherOptions)) {
-                $otherOptions = [$otherOptions];
-            }
-        @endphp
-        @foreach ($otherOptions as $group_id)
-            @php $g = \App\Models\Group::find($group_id); @endphp
-            @if ($g)
-                <option value="{{ $group_id }}">{{ $g->name }}</option>
-            @endif
+        @foreach ($selected as $group_id)
+            <option value="{{ $group_id }}" selected="selected" role="option" aria-selected="true" role="option">
+                {{ \App\Models\Group::find($group_id)->name }}
+            </option>
         @endforeach
     @endisset
+    @isset($otherOptions)
+        @foreach ($otherOptions as $group_id)
+            <option value="{{ $group_id }}" role="option" aria-selected="true" role="option">
+                {{ \App\Models\Group::find($group_id)->name }}
+            </option>
+        @endforeach
+    @endisset
+    @if ($group_id = old($fieldname, isset($item) ? $item->{$fieldname} : ''))
+        <option value="{{ $group_id }}" selected="selected" role="option" aria-selected="true" role="option">
+            {{ \App\Models\Group::find($group_id) ? \App\Models\Group::find($group_id)->name : '' }}
+        </option>
+    @endif
 </select>
