5e75190d7926e0fdbc35e723d525d71289ba0dbc
JonasGoseberg <jonas.goseberg@imbus.de>
2025-10-20 13:14:35 +0200

    fixed Routes

--- routes/web/predefined-filters.php
+++ routes/web/predefined-filters.php
@@ -4,18 +4,10 @@ use App\Http\Controllers\PredefinedFilterController;
 use Illuminate\Support\Facades\Route;
 use Tabuna\Breadcrumbs\Trail;
 
-Route::group([ 'prefix' => 'predefined_filters','middleware' => ['auth'] ], function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('predefined-filters', [PredefinedFilterController::class, 'index'])->name('predefined-filters.index');
     Route::get('predefined-filters/{filter}', [PredefinedFilterController::class,'view'])
           ->name('predefined-filters.view');
     Route::delete('predefined-filters/{id}', [PredefinedFilterController::class, 'destroy'])->name('predefined-filters.destroy');
 });
-
-Route::resource('predefined-filters', PredefinedFilterController::class,
-    [
-        'middleware' => ['auth'],
-        'except' => ['show']
-    ]
-);
-
 
