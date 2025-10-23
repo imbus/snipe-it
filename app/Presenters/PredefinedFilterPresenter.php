21ced12d7c7ac1c2edb23f0310709335199af0f4
JonasGoseberg <jonas.goseberg@imbus.de>
2025-10-23 07:39:36 +0200

    added blank group column

--- app/Presenters/PredefinedFilterPresenter.php
+++ app/Presenters/PredefinedFilterPresenter.php
@@ -63,6 +63,14 @@ class PredefinedFilterPresenter extends Presenter
                 'visible' => true,
                 'formatter' => 'usersLinkObjFormatter',
             ], [
+                'field' => 'groups',
+                'searchable' => true,
+                'sortable' => false,
+                'switchable' => true,
+                'title' => trans('general.groups'),
+                'visible' => true,
+                'formatter' => 'groupsFormatter',
+            ], [
                 'field' => 'created_at',
                 'searchable' => true,
                 'sortable' => true,
