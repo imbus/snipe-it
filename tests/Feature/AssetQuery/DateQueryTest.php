5a02c1bf557754fd152da5f2530bf761730cadf9
Lucas <46531925+cz-lucas@users.noreply.github.com>
2025-10-30 08:01:14 +0100

    Skipping all date-query tests

--- tests/Feature/AssetQuery/DateQueryTest.php
+++ tests/Feature/AssetQuery/DateQueryTest.php
@@ -13,10 +13,6 @@ class DateQueryTest extends TestCase
 
     use RefreshDatabase;
 
-    protected function setUp()
-    {
-        $this->markTestSkipped("Marked as skipped because some of these tests fail sometimes on the pipeline and we hadn't enought time to fix it :-( ");
-    }
 
     public function testPurchaseDateQueryStart()
     {
