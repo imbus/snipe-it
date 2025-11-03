9f4048af5d9b10482a6e49580c897acedf74cb1b
JonasGoseberg <jonas.goseberg@imbus.de>
2025-11-03 09:46:08 +0100

    SIT-125_hotfix language de-if

---
+++ resources/lang/de-if/admin/predefinedFilters/general.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'predefined_filter'     => 'TEST2',
+    'is_public'             => 'Öffentlich',
+    'object_type'           => 'Filtertyp',
+    'info'                  => 'Info',
+];
