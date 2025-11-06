232b206c1c6ae9a05ea0cd97b22c9dfe8732f865
JonasGoseberg <jonas.goseberg@imbus.de>
2025-11-06 07:51:37 +0100

    removed logs

--- app/Http/Controllers/Api/PredefinedFilterController.php
+++ app/Http/Controllers/Api/PredefinedFilterController.php
@@ -12,7 +12,6 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Helpers\Helper;
-use Log;
 
 class PredefinedFilterController extends Controller
 {
