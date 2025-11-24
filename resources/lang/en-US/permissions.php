fe48489bf8ceb93d192cc41f99816be93ded6704
JonasGoseberg <jonas.goseberg@imbus.de>
2025-11-24 08:35:26 +0100

    adjusted l10n for advancedsearch

--- resources/lang/en-US/permissions.php
+++ resources/lang/en-US/permissions.php
@@ -39,6 +39,11 @@ return [
         'note' => 'Grants access to the Assets section of the application. ',
     ],
 
+    'advancedsearch' => [
+        'name' => 'Advanced Search',
+        'note' => 'Grants access to the advanced search section of the Application. Be aware, a user has the rights to load all results from all dropdowns.'
+    ],
+
     'assetsview' => [
         'name' => 'View Assets',
         'note' => 'Note that users with this permission will also be able to see (not modify or delete) files uploaded to the asset model as well. This is to make it easier to share common documents like user manuals across assets without having to upload them to every asset, and to avoid having to grant the user permission to modify asset files.',
