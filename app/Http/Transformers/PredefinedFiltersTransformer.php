4494f0088109550c0b9a7ea7ebaaa85bc458fc30
JonasGoseberg <jonas.goseberg@imbus.de>
2025-12-01 14:13:54 +0100

    fix variable names to match other commits

--- app/Http/Transformers/PredefinedFiltersTransformer.php
+++ app/Http/Transformers/PredefinedFiltersTransformer.php
@@ -56,11 +56,11 @@ class PredefinedFiltersTransformer
             $array['groups'] = null;
         }
 
-        $permissions_array = [];
+        $permissionsArray = [];
 
         $permissionsArray['available_actions'] = [
-            'update' => $filter->created_by === auth()->id() || $filter->userHasPermission(auth()->user(), 'edit'),
-            'delete' => $filter->created_by === auth()->id() || $filter->userHasPermission(auth()->user(), 'delete')
+            'update' => $filter->userHasPermission(auth()->user(), 'edit'),
+            'delete' => $filter->userHasPermission(auth()->user(), 'delete')
         ];
         return $array += $permissionsArray;
     }
