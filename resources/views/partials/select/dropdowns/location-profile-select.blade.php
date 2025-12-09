aab7b63ffe3662e572536f451606c797c7357559
Lucas <46531925+cz-lucas@users.noreply.github.com>
2025-12-09 07:59:51 +0100

    Fixed 500-error on profile-edit-page
    Added a check to set a fallback-value if the fieldname isn't set

--- resources/views/partials/select/dropdowns/location-profile-select.blade.php
+++ resources/views/partials/select/dropdowns/location-profile-select.blade.php
@@ -1,12 +1,29 @@
+{{-- Some pages won't set the $fieldname. If this is the case the fallback-value of "default_location_profile_select" is used. --}}
 <select class="js-data-ajax"
     data-endpoint="locations"
-    data-placeholder="{{ trans('general.select_location') }}" name="location_id" style="width: 100%" id="{{ isset($select_id) ? $select_id : $fieldname . '_location_profile_select' }}" aria-label="location_id">
-    data-tags="{{ isset($allow_tags) && $allow_tags ? 'true' : 'false' }}"
-    @if ($location_id = old('location_id', (isset($user)) ? $user->location_id : ''))
-    <option value="{{ $location_id }}" selected="selected" role="option" aria-selected="true" role="option">
-        {{ (\App\Models\Location::find($location_id)) ? \App\Models\Location::find($location_id)->name : '' }}
-    </option>
+
+    @isset($fieldname)
+        data-placeholder="{{ trans('general.select_location') }}"
+    @endisset
+
+    name="location_id"
+    style="width: 100%"
+
+    id="{{ $select_id ?? (isset($fieldname) ? $fieldname . '_location_profile_select' : 'default_location_profile_select') }}"
+
+    aria-label="location_id"
+
+    @isset($fieldname)
+        data-tags="{{ !empty($allow_tags) ? 'true' : 'false' }}"
+    @endisset
+>
+
+    @if ($location_id = old('location_id', isset($user) ? $user->location_id : ''))
+        <option value="{{ $location_id }}" selected aria-selected="true" role="option">
+            {{ optional(\App\Models\Location::find($location_id))->name }}
+        </option>
     @else
-    <option value="" role="option">{{ trans('general.select_location') }}</option>
+        <option value="" role="option">{{ trans('general.select_location') }}</option>
     @endif
+
 </select>
